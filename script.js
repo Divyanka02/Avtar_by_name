4b70e546b7d3206f8d499e7a66a276091c29a931
Divyanka02 <85795778+Divyanka02@users.noreply.github.com>
2021-10-05 20:02:58 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,1 @@
-$(document).ready(function(){
-    $('button').click(function(){
-     const name = $('#nameid').val();
-     alert(name+"Your Avtar ");
-     $('.imgchange').attr('src',`https://joeschmoe.io/api/v1/${name}`);
-     const b = $('.imgchange');
-     console.log(b);
-  
-    });
-   });
+
